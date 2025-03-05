ef92177f064213f5881f24ee6df7c865421d28ac
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-03-05 11:39:06 +0700

    Luas Persegi C++

---
+++ luaspersegi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    float panjang, lebar, luas;
+    cout << "Masukkan panjangnya = ";
+    cin >> panjang;
+    cout << "Masukkan lebarnya = ";
+    cin >> lebar;
+    luas = panjang * lebar;
+    cout << "Luas persegi panjangnya = " << luas;
+}
